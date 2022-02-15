02def05dba8cd262542f34b68f1672723a8607b6
shikoryak <shikoryak_oksana@ukr.net>
2022-02-15 19:14:28 +0200

    add mobile header

---
+++ js/heder-navbar.js
@@ -0,0 +1,8 @@
+const buttonMenu = document.getElementById('buttonMenu');
+const headerNavbar = document.getElementById('headerNavbar');
+buttonMenu.onclick = function () {
+    headerNavbar.classList.add('open');
+}
+headerNavbar.onclick = function() {
+    headerNavbar.classList.remove('open');
+}
